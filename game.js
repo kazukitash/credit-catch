e2c0ac79b690a074fa7e55d20397103648029029
kazukitash <Takahashi@undapt.jp>
2013-11-06 14:10:26 +0900

    画像を表示してみよう

--- game.js
+++ game.js
@@ -9,8 +9,29 @@ window.onload = function(){
   //ゲームのfpsを指定
   molTaniCatch.fps = 30;
 
+  //mol.pngをロードします
+  molTaniCatch.preload("img/mol.png")
+
   //この中にゲームを記述していく
   molTaniCatch.onload = function(){
+
+    //自機オブジェクト（以下、自機）をSpriteクラスから作る
+    var jiki = new Sprite(151, 169);
+
+    //自機に画像を設定する
+    jiki.image = molTaniCatch.assets["img/mol.png"];
+
+    //自機のフレームを指定
+    jiki.frame = 0;
+
+    //自機のx座標
+    jiki.x = 120;
+
+    //自機のy座標
+    jiki.y = 300;
+
+    //自機をゲーム画面に表示する
+    molTaniCatch.rootScene.addChild(jiki);
 
   }
 
