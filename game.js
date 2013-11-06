4fe6b5005f29f44b8d1d5f02a006f4bee7f0d4d9
kazukitash <Takahashi@undapt.jp>
2013-11-06 15:08:42 +0900

    Spriteクラスを継承して自機クラスを作ろう

--- game.js
+++ game.js
@@ -1,5 +1,39 @@
 enchant();  //enchantを利用するためのおまじない（enchant.js本体やクラスをエクスポートしている）
 
+//Spriteクラスを継承したJikiクラスをつくる
+var Jiki = enchant.Class.create(Sprite, {
+  //インスタンスオブジェクトを生成したときに実行される
+  initialize: function(){
+    //おまじない
+    Sprite.call(this, 151, 169);
+
+    //自機に画像を設定する
+    this.image = molTaniCatch.assets["img/mol.png"];
+
+    //自機のフレームを指定
+    this.frame = 0;
+
+    //自機のx座標
+    this.x = 120;
+
+    //自機のy座標
+    this.y = 300;
+  },
+
+  ontouchmove: function(e){
+    //動きの左右を判定して自機の画像を変更する
+    if(e.x - this.width / 2 > this.x){
+      this.frame = 0;
+    } else if(e.x - this.width / 2 < this.x){
+      this.frame = 1;
+    }
+
+    //touchmoveイベントにおいて関数の第一引数eはクリックした座標を返してくれる
+    this.x = e.x - this.width / 2
+    this.y = e.y - this.height / 2
+  }
+});
+
 //ブラウザがページを読み込んだときに実行する関数
 window.onload = function(){
 
@@ -20,34 +54,8 @@ window.onload = function(){
   //この中にゲームを記述していく
   molTaniCatch.onload = function(){
 
-    //自機オブジェクト（以下、自機）をSpriteクラスから作る
-    var jiki = new Sprite(151, 169);
-
-    //自機に画像を設定する
-    jiki.image = molTaniCatch.assets["img/mol.png"];
-
-    //自機のフレームを指定
-    jiki.frame = 0;
-
-    //自機のx座標
-    jiki.x = 120;
-
-    //自機のy座標
-    jiki.y = 300;
-
-    //自機を動かす
-    jiki.addEventListener("touchmove", function(e){
-      //動きの左右を判定して自機の画像を変更する
-      if(e.x - jiki.width / 2 > jiki.x){
-        jiki.frame = 0;
-      } else if(e.x - jiki.width / 2 < jiki.x){
-        jiki.frame = 1;
-      }
-
-      //touchmoveイベントにおいて関数の第一引数eはクリックした座標を返してくれる
-      jiki.x = e.x - jiki.width / 2
-      jiki.y = e.y - jiki.height / 2
-    });
+    //自機オブジェクト（以下、自機）をJikiクラスから作る
+    var jiki = new Jiki();
 
     //ゲームの背景画像を表示する
     var haikei = new Sprite(YokoHaba, TateHaba);
